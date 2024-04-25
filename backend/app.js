ef55e5d14e5cbd626910d1fe3fb7975fe70db42f
Abbas-coding <abbasfakhruddin205@gmail.com>
2024-04-25 06:09:07 +0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,7 @@ const path = require('path');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}));
+app.use(cors());
 app.use('/test', (req,res)=>{
     res.send("Hello World!");
 });
